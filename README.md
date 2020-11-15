8716d41f7a6d23d1ccba117d6b9cd0228a070507
NewBobPlus <rschuhler@hotmail.fr>
2020-11-16 00:51:09 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hidden-files
+
+Extension for Chrome to hide files by extensions on github repos.
+It also works with Octotree (an extension for a tree view of the code) :
+https://chrome.google.com/webstore/detail/octotree-github-code-tree/bkhaagjahfmjljalopjnoealnfndnagc
+
+I don't want to pay the registration fees for the store, so here is how you can install it 👀 :
+![render](/screenshot.png)
+- Enable Developer mode
+- Clone the repo and load it
+- Disable Developer mode
+- You can now go to the extension's options and choose what files you want to hide
--- hide.js
+++ hide.js
@@ -1,48 +1,50 @@
 
-//TODO: Window.requestAnimationFrame()?
+(() => {
 
-const hide = async () => {
+  const hide = async () => {
 
-  chrome.storage.sync.get('extensions', (data) => {
-    const extensions = data.extensions;
+    chrome.storage.sync.get('extensions', (data) => {
+      const extensions = data.extensions;
 
-    const filterExt = (element, name) => {
-      for(let extension of extensions)
-      if(name.endsWith(extension)) element.remove();
-    };
+      const filterExt = (element, name) => {
+        for(let extension of extensions)
+        if(name.endsWith(extension)) element.remove();
+      };
 
-    const files = document.querySelector('[aria-labelledby="files"]');
-    if(files) {
-      let children = Array.from(files.children);
-      let len = children.length;
+      const files = document.querySelector('[aria-labelledby="files"]');
+      if(files) {
+        let children = Array.from(files.children);
+        let len = children.length;
 
-      for(let i=1; i<len; i++) {
-        let name = children[i].querySelector('[title]').innerText;
-        filterExt(children[i], name);
+        for(let i=1; i<len; i++) {
+          let name = children[i].querySelector('[title]').innerText;
+          filterExt(children[i], name);
+        }
       }
-    }
 
-    chrome.storage.sync.get('octotree', (data) => {
-      if(!data.octotree) return;
+      chrome.storage.sync.get('octotree', (data) => {
+        if(!data.octotree) return;
 
-      const octotree = document.querySelector('.octotree-view-body');
-      if(!octotree) return;
-      children = Array.from(octotree.querySelectorAll('[title]'));
-      len = children.length;
-      if(len == 0) return;
+        const octotree = document.querySelector('.octotree-view-body');
+        if(!octotree) return;
+        children = Array.from(octotree.querySelectorAll('[title]'));
+        len = children.length;
+        if(len == 0) return;
 
-      for(let i=1; i<len; i++) {
-        let name = children[i].getAttribute('title');
-        filterExt(children[i], name);
-      }
+        for(let i=1; i<len; i++) {
+          let name = children[i].getAttribute('title');
+          filterExt(children[i], name);
+        }
+      });
     });
-  });
-};
+  };
 
-const loop = async () => {
-  hide();
-  await new Promise(resolve => setTimeout(resolve, 200));
+  const loop = async () => {
+    hide();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    loop();
+  };
+
   loop();
-};
 
-loop();
+})();
