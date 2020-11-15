84720cacd0c8fab28391bccd6370b2f27becfb2d
NewBobPlus <rschuhler@hotmail.fr>
2020-11-16 00:04:09 +0100

    it works ✔

--- hide.js
+++ hide.js
@@ -1,58 +1,48 @@
 
-(() => {
+//TODO: Window.requestAnimationFrame()?
 
-  const files = document.querySelector('[aria-labelledby="files"]');
-  if(!files) return;
+const hide = async () => {
 
   chrome.storage.sync.get('extensions', (data) => {
     const extensions = data.extensions;
 
-    let filtered = 0;
     const filterExt = (element, name) => {
-      for(let extension of extensions) {
-          if(name.endsWith(extension)) element.remove();
-          filtered++;
-      }
+      for(let extension of extensions)
+      if(name.endsWith(extension)) element.remove();
     };
 
-    let children = Array.from(files.children);
-    let len = children.length;
+    const files = document.querySelector('[aria-labelledby="files"]');
+    if(files) {
+      let children = Array.from(files.children);
+      let len = children.length;
 
-    for(let i=1; i<len; i++) {
-      let name = children[i].querySelector('[title]').innerText;
-      filterExt(children[i], name);
+      for(let i=1; i<len; i++) {
+        let name = children[i].querySelector('[title]').innerText;
+        filterExt(children[i], name);
+      }
     }
 
-    let goal = filtered;
-    filtered = 0;
-
-    const processOctotree = () => {
-      const check = () => {
-        const octotree = document.querySelector('.octotree-view-body');
-        if(!octotree) return;
-        children = octotree.querySelectorAll('[title]');
-        if(!children) return;
-
-        for(let i=1; i<children.length; i++) {
-          let name = children[i].getAttribute('title');
-          filterExt(children[i], name);
-        }
-
-        return filtered == goal;
-      };
-
-      const loop = async () => {
-        if(check()) return;
-        await new Promise(resolve => setTimeout(resolve, 100));
-        loop();
-      };
-
-      loop();
-    };
-
     chrome.storage.sync.get('octotree', (data) => {
-      if(data.octotree) processOctotree();
+      if(!data.octotree) return;
+
+      const octotree = document.querySelector('.octotree-view-body');
+      if(!octotree) return;
+      children = Array.from(octotree.querySelectorAll('[title]'));
+      len = children.length;
+      if(len == 0) return;
+
+      for(let i=1; i<len; i++) {
+        let name = children[i].getAttribute('title');
+        filterExt(children[i], name);
+      }
     });
   });
+};
 
-})();
+const loop = async () => {
+  hide();
+  await new Promise(resolve => setTimeout(resolve, 200));
+  loop();
+};
+
+loop();
